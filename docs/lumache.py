4aa45037a9a7df9c720fe527e52458d62bd8a3f8
fabrizio.i <fabrizio.i@alfa.gov.it>
2026-04-07 15:02:36 +0200

    TOML?

--- docs/lumache.py
+++ docs/lumache.py
@@ -13,7 +13,7 @@ class InvalidKindError(Exception):
 def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
-
+TOML?
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
     :raise lumache.InvalidKindError: If the kind is invalid.
